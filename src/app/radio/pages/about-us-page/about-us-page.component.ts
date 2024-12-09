941054f7a16601f4ba393fac806405cdecac2cc4
Charlie Cárdenas <charlie.act7@gmail.com>
2024-12-09 12:22:50 -0500

    Actualización del front

--- src/app/radio/pages/about-us-page/about-us-page.component.ts
+++ src/app/radio/pages/about-us-page/about-us-page.component.ts
@@ -9,9 +9,7 @@ import { MediaElement } from '../../interfaces/media-element.interface';
 })
 export class AboutUsPageComponent {
   public mediaTeamMediaLab:MediaElement[] = [
-    {id: '01',title:'Técnico de Laboratorio', description: 'LOURDES KATHERINE QUEZADA LOAIZA', imgSrc: 'https://e7.pngegg.com/pngimages/820/233/png-clipart-management-service-business-consultant-employment-students-company-service.png'},
-    {id: '02',title:'Técnico de Laboratorio', description: 'LOURDES KATHERINE QUEZADA LOAIZA', imgSrc: 'https://e7.pngegg.com/pngimages/820/233/png-clipart-management-service-business-consultant-employment-students-company-service.png'},
-    {id: '03',title:'Técnico de Laboratorio', description: 'LOURDES KATHERINE QUEZADA LOAIZA', imgSrc: 'https://e7.pngegg.com/pngimages/820/233/png-clipart-management-service-business-consultant-employment-students-company-service.png'},
-    {id: '04',title:'Técnico de Laboratorio', description: 'LOURDES KATHERINE QUEZADA LOAIZA', imgSrc: 'https://e7.pngegg.com/pngimages/820/233/png-clipart-management-service-business-consultant-employment-students-company-service.png'}
+    {id: '01',title:'Directora del departamento de Ciencias de la Comunicación de UTPL', description: 'Ana Maria Beltrán', imgSrc: 'https://e7.pngegg.com/pngimages/820/233/png-clipart-management-service-business-consultant-employment-students-company-service.png'},
+    {id: '02',title:'Técnico de Radio UTPL', description: 'Lourdes Quezada Loaiza', imgSrc: 'https://e7.pngegg.com/pngimages/820/233/png-clipart-management-service-business-consultant-employment-students-company-service.png'},
   ]
 }
--- src/app/radio/pages/home-page/home-page.component.ts
+++ src/app/radio/pages/home-page/home-page.component.ts
@@ -26,9 +26,12 @@ export class HomePageComponent implements OnInit{
   public recentProject3: Project | null = null;
 
   public mediaElementCarousel:MediaElement[] = [
-    {id: '01', title: 'Una radio que es parte de la comunidad', imgSrc: 'https://firebasestorage.googleapis.com/v0/b/radioutpl.appspot.com/o/media%2Fimage_carrousel.png?alt=media&token=2e17e727-a307-48e1-82fa-ea578c449a69', linkContent: 'https://www.youtube.com/watch?v=Lm77VCkf_do'},
-    {id: '02', title: 'Second Image', imgSrc: 'https://images.alphacoders.com/133/1330482.png'},
-    {id: '03', title: 'Tree Image', imgSrc: 'https://firebasestorage.googleapis.com/v0/b/radioutpl.appspot.com/o/media%2Fimage_carrousel.png?alt=media&token=2e17e727-a307-48e1-82fa-ea578c449a69'}
+    {id: '01', title: 'En Radio UTPL, cada voz cuenta. ¡Sintoniza y sé parte de nuestra comunidad académica!', imgSrc: 'https://firebasestorage.googleapis.com/v0/b/radioutpl.appspot.com/o/carousel_home%2F1%202.jpg?alt=media&token=82877c38-c84d-433e-950c-54e8edf68e42'},
+    {id: '02', title: 'Desde el campus hasta tu hogar, Radio UTPL te conecta con la esencia de nuestra universidad', imgSrc: 'https://firebasestorage.googleapis.com/v0/b/radioutpl.appspot.com/o/carousel_home%2F2.jpg?alt=media&token=9e697573-662f-40e5-92fe-1731b03a3bfd'},
+    {id: '03', title: 'Aquí en Radio UTPL, celebramos el talento y las historias de nuestros estudiantes. ¡Tu voz es nuestra inspiración!', imgSrc: 'https://firebasestorage.googleapis.com/v0/b/radioutpl.appspot.com/o/carousel_home%2F3.jpg?alt=media&token=04baf205-3ba5-4611-bc10-9084c5a1cee3'},
+    {id: '04', title: 'Con cada programa, fortalecemos los lazos entre la universidad y la comunidad. ¡Gracias por ser parte de Radio UTPL!', imgSrc: 'https://firebasestorage.googleapis.com/v0/b/radioutpl.appspot.com/o/carousel_home%2F4.jpg?alt=media&token=c64bf7b4-db7c-48b0-9602-f07bdd4bf8ac'},
+    {id: '05', title: 'En Radio UTPL, la música y el conocimiento se entrelazan. ¡Súmate a nuestra programación y descubre lo que somos!', imgSrc: 'https://firebasestorage.googleapis.com/v0/b/radioutpl.appspot.com/o/carousel_home%2F5.jpg?alt=media&token=72c6548b-dc6c-4d91-811b-b283996da987'},
+    {id: '06', title: 'Tu comunidad académica, tu música, tu radio. ¡Bienvenidos a Radio UTPL!', imgSrc: 'https://firebasestorage.googleapis.com/v0/b/radioutpl.appspot.com/o/carousel_home%2F6.png?alt=media&token=3dc5fd46-5bca-4806-89ae-43f28ff427f9'}
   ];
 
   private unsubscribe$ = new Subject<void>();
